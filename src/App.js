5ad505cb5092546f463aee2d40378ccc705d0ffd
hafsa <hfsyln93@gmail.com>
2022-10-31 00:39:57 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Login from "./pages/login/Login";
 import NavBar from "./components/navbar/NavBar" ;
-
+import AppRouter from './router/AppRouter';
 import Home from "./pages/home/Home"
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import About from './pages/about/About';
 import RecipeCard from './pages/home/RecipeCard';
 
@@ -16,16 +16,9 @@ function App() {
 
   return (
     <div>
-        <BrowserRouter>
-    
-    <Routes>
-        <Route path="/home" element={<Home />} />
-      
-    </Routes>
+    <BrowserRouter>
+        <AppRouter/>
     </BrowserRouter>
-      
-      
-    <Login/>
 
     </div>
   )
--- src/pages/home/RecipeCard.jsx
+++ src/pages/home/RecipeCard.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+//import { useState } from 'react';
 
 
 const RecipeCard = ({key, recipe}) => {
 
-    //let navigate = useNavigate();
+    //const [aç, setAç]= useState(true)
+    let navigate = useNavigate();
     console.log(recipe)
     console.log(recipe.recipe.image)
+
+   
 
     return (
         <div>
         <Card className='text-center d-flex flex-column gap-3 m-4 bg-secondary' key={key} style={{ width: '22vw', height: "55vh"}} >
           <Card.Img className='p-2' variant="top" style={{ height: '35vh' }} src={recipe?.recipe.image}/>
             <Card.Title>{recipe?.recipe.label}</Card.Title>
-            <Button className='mx-auto my-3 CardBtn' style={{width:"10vw"}} >View More</Button>
+            <Button className='mx-auto my-3 CardBtn' style={{width:"10vw"}} onClick={()=>{navigate("/details", { state: recipe });}} >View More</Button>
         </Card>
         </div>
-       
+         
   )
 }
 
