9e0175acbe19b1b67f3209213c75254734a5d45e
hafsa <hfsyln93@gmail.com>
2022-10-31 16:50:01 +0300

    router

--- src/pages/home/RecipeCard.jsx
+++ src/pages/home/RecipeCard.jsx
@@ -8,7 +8,7 @@ import Card from 'react-bootstrap/Card';
 const RecipeCard = ({key, recipe}) => {
 
     //const [aç, setAç]= useState(true)
-    let navigate = useNavigate();
+    const navigate = useNavigate();
     console.log(recipe)
     console.log(recipe.recipe.image)
 
@@ -19,7 +19,7 @@ const RecipeCard = ({key, recipe}) => {
         <Card className='text-center d-flex flex-column gap-3 m-4 bg-secondary' key={key} style={{ width: '22vw', height: "55vh"}} >
           <Card.Img className='p-2' variant="top" style={{ height: '35vh' }} src={recipe?.recipe.image}/>
             <Card.Title>{recipe?.recipe.label}</Card.Title>
-            <Button className='mx-auto my-3 CardBtn' style={{width:"10vw"}} onClick={()=>{navigate("/details", { state: recipe });}} >View More</Button>
+            <Button className='mx-auto my-3 CardBtn' style={{width:"10vw"}} onClick={()=>{navigate("/details", { state: recipe });}} >View More</Button> 
         </Card>
         </div>
          
