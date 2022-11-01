76ffd23cdf03d74b2efadc049f7ae473ee20d3b3
hafsa <hfsyln93@gmail.com>
2022-11-01 21:40:04 +0300

    fınısh

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,19 +1,48 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
 import NavBar from "../../components/navbar/NavBar"
-import meal2 from "../../assets/meal2.svg"
+import diet from "../../assets/diet.svg"
 
 const Details = () => {
 
-  const {state} =useLocation()
-  console.log(state)
+  const {state} = useLocation()
+  console.log(state.recipe)
   return (
     <div>
       
-    <NavBar/>
+        <NavBar/>
 
-
-    <p>nvbfgdkjÇHSKJdZVMNCBÇKGRSDJZFNBXköhGJngbN</p>
+        <div className='detDiv'>
+            <div className="d-flex m-5 justify-content-center gap-5">
+                <h2 className='mt-5'>{state.recipe.label}</h2>
+                <img src={diet} className="mt-4" style={{with:"30vw", height:"20vh"}} />
+            </div>
+        </div>
+        <div className="d-flex gap-5 justify-content-center">
+          <div className='mt-5'>
+                <p>
+                   Calorie : {Math.trunc(state.recipe.calories)}
+                </p>
+                <p>
+                  Total Weight:{state.recipe.totalWeight}
+                </p>
+                <p>
+                  CA Quantity : {Math.round(state.recipe.totalNutrients.CA.quantity)}
+                </p>
+                <p>
+                  KCAL: {Math.round(state.recipe.totalDaily.ENERC_KCAL.quantity)}
+                </p>
+            </div>
+            <img src={state.recipe.image} alt=""  style={{ height:"40vh"}}/> 
+              <div className='mt-5'>
+                  {state.recipe.ingredientLines.map((item) => {
+                    return (
+                        <p>-{item}</p>
+                    );
+                    })} 
+                </div>
+            
+      </div>
     </div>
 
   )
